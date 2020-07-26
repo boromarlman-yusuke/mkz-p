0843548a41a17909d7b58212eebcc0b0e9db50b1
yusuke <boromarlman@gmail.com>
2020-07-26 06:58:25 +0000

    メインページを編集した

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,41 @@
 <template>
   <v-layout
     column
-    justify-center
-    align-center
   >
     <v-flex
       xs12
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+    <v-carousel
+      cycle
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+        :key="i"
+      >
+        <v-sheet
+          :color="colors[i]"
+          height="100%"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <div class="display-3">{{ slide }} </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          ここはタイトル
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <p>なんかテキスト</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -84,14 +52,22 @@
   </v-layout>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang="ts">
+  import {Component, Prop, Vue} from "vue-property-decorator";
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  @Component
+  export default class index extends Vue {
+
+    private colors = [
+      'indigo',
+      'warning',
+      'pink darken-2'
+    ]
+
+    private slides = [
+      '税理士事務所',
+      'あいさつ',
+      'ひとこと'
+    ]
   }
-}
 </script>
