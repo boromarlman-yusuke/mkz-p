6daad12739d20ff448a1d76d2d64ffb21ac77500
yusuke <boromarlman@gmail.com>
2020-07-26 08:01:15 +0000

    googleMapを埋め込んだ

--- pages/index.vue
+++ pages/index.vue
@@ -48,6 +48,16 @@
           </v-btn>
         </v-card-actions>
       </v-card>
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1612.6932379118032!2d136.17780975827426!3d36.059676463337304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8bf2a9efda78b%3A0x9d76b6518811ffe1!2z54uQ5qmL5YWs5ZyS!5e0!3m2!1sja!2sjp!4v1595750092590!5m2!1sja!2sjp"
+        width="600"
+        height="450"
+        frameborder="0"
+        style="border:0;"
+        allowfullscreen=""
+        aria-hidden="false"
+        tabindex="0">
+      </iframe>
     </v-flex>
   </v-layout>
 </template>
