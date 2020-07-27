407a5b359c16c9a1a70cf58787417f89137c3576
yusuke <boromarlman@gmail.com>
2020-07-27 10:06:44 +0000

    topページの内容を編集した - map - 業務内容

--- pages/index.vue
+++ pages/index.vue
@@ -15,27 +15,35 @@
       <v-carousel-item
         v-for="(slide, i) in slides"
         :key="i"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
       >
         <v-sheet
           :color="colors[i]"
           height="100%"
         >
+
           <v-row
             class="fill-height"
             align="center"
             justify="center"
           >
-            <div class="display-3">{{ slide }} </div>
+            <!-- <div class="display-3">{{ slide }} </div> -->
+            <div class="display-3">aiueo</div>
           </v-row>
+
         </v-sheet>
+      
       </v-carousel-item>
     </v-carousel>
       <v-card>
         <v-card-title class="headline">
-          ここはタイトル
         </v-card-title>
+
         <v-card-text>
-          <p>なんかテキスト</p>
+          <p>福井で生まれ福井で育ち育てられた税理士です。</p>
+          <p>中小企業様、個人事業主様を全面的にサポートさせていただきます。</p>
+          <p>(対応エリア：福井県内、石川県加賀地方、その他の地域については、応談)</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
@@ -48,16 +56,103 @@
           </v-btn>
         </v-card-actions>
       </v-card>
-      <iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1612.6932379118032!2d136.17780975827426!3d36.059676463337304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8bf2a9efda78b%3A0x9d76b6518811ffe1!2z54uQ5qmL5YWs5ZyS!5e0!3m2!1sja!2sjp!4v1595750092590!5m2!1sja!2sjp"
-        width="600"
-        height="450"
-        frameborder="0"
-        style="border:0;"
-        allowfullscreen=""
-        aria-hidden="false"
-        tabindex="0">
-      </iframe>
+      <v-card>
+        <v-card-title class="headline">
+          サービスについて
+        </v-card-title>
+        <v-card-text>
+          <p>当事務所は、経営者皆様がすべき価値ある仕事に専念いただきたいと考えており、</p>
+          <p>税理士 松島和彦は元国税職員としての経験を活かし、全面的にサポート</p>
+          <p>させていただきますのでご安心ください。</p>
+        </v-card-text>
+      </v-card>
+      <v-row>
+        <v-col>
+          <v-card min-height="400px">
+            <v-card-title class="headline">
+              創業
+            </v-card-title>
+            <v-card-text>
+              <ul>
+                <li>創業、会社設立に関する相談、税務上のアドバイス及び届け出関係</li>
+                <li>相続・贈与・譲渡(資産税関係)</li>
+                <li>相続税の申告納税が必要かのシミュレーション</li>
+                <li>申告から納税までのスケジュールの相談</li>
+                <li>相続税申告書の作成及び申告</li>
+                <li>贈与について</li>
+                <li>贈与を行うまでの税務関係の相談(最良の贈与の仕方等、贈与を行う前に、まずはご相談ください。)</li>
+                <li>贈与税申告書の作成及び申告</li>
+                <li>資産関係の譲渡所得についての相談、申告書の作成及び申告</li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card min-height="400px">
+            <v-card-title class="headline">
+              事業継承
+            </v-card-title>
+            <v-card-text>
+              <ul>
+                <li>事業承継及び事業の譲渡・買収に関する相談等</li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card min-height="400px">
+            <v-card-title class="headline">
+              税務・会計
+            </v-card-title>
+            <v-card-text>
+              <ul>
+                <li>税務に関する各種申告書</li>
+                <li>届出書等の作成及び申告</li>
+                <li>経理事務の指導、記帳代行</li>
+                <li>会計、給与、システムの導入指導年末調整</li>
+                <li>償却資産の申告書作成及び申告</li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card min-height="400px">
+            <v-card-title class="headline">
+              相談
+            </v-card-title>
+            <v-card-text>
+              <ul>
+                <li>税務全般に関する相談</li>
+                <li>税務調査の対応</li>
+                <li>キャッシュフロー計算書の作成による管理・経営のアドバイス</li>
+                <li>(初回相談は、無料とさせていただいております。お気軽にご相談ください。)</li>
+                <li></li>
+                <li></li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-card>
+        <v-card-title>
+
+        </v-card-title>
+        <v-card-text>
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1612.6932379118032!2d136.17780975827426!3d36.059676463337304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8bf2a9efda78b%3A0x9d76b6518811ffe1!2z54uQ5qmL5YWs5ZyS!5e0!3m2!1sja!2sjp!4v1595750092590!5m2!1sja!2sjp"
+            width="600"
+            height="450"
+            frameborder="0"
+            style="border:0;"
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0">
+          </iframe>
+        </v-card-text>
+      </v-card>
+
     </v-flex>
   </v-layout>
 </template>
@@ -75,7 +170,7 @@
     ]
 
     private slides = [
-      '税理士事務所',
+      '〇〇税理士事務所',
       'あいさつ',
       'ひとこと'
     ]
