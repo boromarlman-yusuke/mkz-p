e44ea367015a19df4dab45aded254a2c0a513428
yusuke <boromarlman@gmail.com>
2020-08-08 14:44:53 +0000

    自前でお問い合わせのデザインを作成するのをやめて、GFを直接埋め込むようにした

--- pages/form.vue
+++ pages/form.vue
@@ -2,89 +2,21 @@
   <v-layout>
     <v-flex class="text-center">
       <div id="app" class="container" v-cloak>
-      <h2>お問い合わせ</h2>
-
-      <transition-group name="fade" tag="div" mode="out-in">
-        <form
-          key="1" 
-          name="gf_form" method="POST" target="hidden_iframe"
-          :action="formdata.doc">
-          <Listitem 
-            v-for="(item, key, index) in formdata.surveys" 
-            :item="item" 
-            :index="index" 
-            :key="index"
-          ></Listitem>
-          <v-btn>送信する</v-btn>        
-        </form>
-        <div key="2">
-          <p>
-            回答を記録しました。<br>
-            ご協力ありがとうございました。
-          </p>
-        </div>
-      </transition-group>
-    </div>
+        <h2>お問い合わせ</h2>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfhiwi5QYOYm_nJo4SN2btj_II7-tU2EZlA8nXb2-wlHupW5Q/viewform?embedded=true" width="640" height="1200" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+      </div>
     </v-flex>
   </v-layout>
 </template>
 
 <script lang="ts">
   import {Component, Prop, Vue} from "vue-property-decorator";
-  import {_Formdata, Survey} from "@/src/vue-gf-data.ts";
-  import Listitem from "@/components/Listitem.vue";
-  import Logo from "@/components/Logo.vue";
   
   @Component
   export default class index extends Vue {
     
+  }
 
-  private formdata: _Formdata = {
-    doc: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfhiwi5QYOYm_nJo4SN2btj_II7-tU2EZlA8nXb2-wlHupW5Q/formResponse",
-    surveys:[
-      {
-        name: 258860309,
-        question: 'メールアドレス',
-        questiontype: 'text',
-        label: 'email',
-        placeholder: 'abcde@example.com'
-        // validate: 'required|email'
-      },
-      {
-        name: 971087384,
-        question: '氏名',
-        questiontype: 'text',
-        label: 'username',
-        placeholder: '山田太郎'
-        // validate: true
-      },
-      {
-        name: 971087384,
-        question: '氏名(カナ)',
-        questiontype: 'text',
-        label: 'username',
-        placeholder: 'ヤマダタロウ'
-        // validate: true
-      },
-      {
-        name: 989251126,
-        question: '電話番号',
-        questiontype: 'text',
-        label: 'phoneNumber',
-        placeholder: '0123456789'
-        // validate: 'required|numeric'
-      },
-      {
-        name: 989251126,
-        question: 'お問い合わせ内容',
-        questiontype: 'textarea',
-        label: 'naiyou',
-        placeholder: '〜の件'
-        // validate: 'required|numeric'
-      }
-    ]
-  };
-}
 </script>
 
 <style>
