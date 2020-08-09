1b9b75ad34cbc16b444819cf9da91e8c6839d5c7
yusuke <boromarlman@gmail.com>
2020-08-09 02:08:55 +0000

    業務紹介カードを"田"の形をやめて縦に一列に表示させるようにした。スマホの対応

--- pages/index.vue
+++ pages/index.vue
@@ -40,106 +40,93 @@
         <p>させていただきますのでご安心ください。</p>
       </v-card-text>
     </v-card>
-
-      <v-row>
-        <v-col>
-          <v-card min-height="400px">
-            <v-card-title class="headline">
-              創業
-            </v-card-title>
-            <v-card-text>
-              <ul>
-                <li>創業、会社設立に関する相談、税務上のアドバイス及び届け出関係</li>
-                <li>相続・贈与・譲渡(資産税関係)</li>
-                <li>相続税の申告納税が必要かのシミュレーション</li>
-                <li>申告から納税までのスケジュールの相談</li>
-                <li>相続税申告書の作成及び申告</li>
-                <li>贈与について</li>
-                <li>贈与を行うまでの税務関係の相談(最良の贈与の仕方等、贈与を行う前に、まずはご相談ください。)</li>
-                <li>贈与税申告書の作成及び申告</li>
-                <li>資産関係の譲渡所得についての相談、申告書の作成及び申告</li>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card min-height="400px">
-            <v-card-title class="headline">
-              事業継承
-            </v-card-title>
-            <v-card-text>
-              <ul>
-                <li>事業承継及び事業の譲渡・買収に関する相談等</li>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-card min-height="400px">
-            <v-card-title class="headline">
-              税務・会計
-            </v-card-title>
-            <v-card-text>
-              <ul>
-                <li>税務に関する各種申告書</li>
-                <li>届出書等の作成及び申告</li>
-                <li>経理事務の指導、記帳代行</li>
-                <li>会計、給与、システムの導入指導年末調整</li>
-                <li>償却資産の申告書作成及び申告</li>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col>
-          <v-card min-height="400px">
-            <v-card-title class="headline">
-              相談
-            </v-card-title>
-            <v-card-text>
-              <ul>
-                <li>税務全般に関する相談</li>
-                <li>税務調査の対応</li>
-                <li>キャッシュフロー計算書の作成による管理・経営のアドバイス</li>
-                <li>(初回相談は、無料とさせていただいております。お気軽にご相談ください。)</li>
-              </ul>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-card>
-        <v-card-title>
-          アクセスマップ
-        </v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col>
-              <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1612.6932379118032!2d136.17780975827426!3d36.059676463337304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8bf2a9efda78b%3A0x9d76b6518811ffe1!2z54uQ5qmL5YWs5ZyS!5e0!3m2!1sja!2sjp!4v1595750092590!5m2!1sja!2sjp"
-                width="600"
-                height="450"
-                frameborder="0"
-                style="border:0;"
-                allowfullscreen=""
-                aria-hidden="false"
-                tabindex="0">
-              </iframe>
-            </v-col>
-            <v-col>
-              <p>お急ぎのご用件の場合は下記の電話番号におかけください。</p>
-              <p>お問い合わせフォームよりご用件を頂いた場合は、メールにて回答後、お電話を差し上げます。</p>
-              <h1>連絡先</h1>
-              <Ul>
-                <li>福井県福井市xxxxxxxxx</li>
-                <li>0123-56-7890</li>
-                <li>aaaaaaaaaaaaaaaaaaaaaa@gmail.com</li>
-                <li>FAX:(123)123-5678</li>
-              </Ul>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
+    <v-card min-height="400px">
+      <v-card-title class="headline">
+        創業
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li>創業、会社設立に関する相談、税務上のアドバイス及び届け出関係</li>
+          <li>相続・贈与・譲渡(資産税関係)</li>
+          <li>相続税の申告納税が必要かのシミュレーション</li>
+          <li>申告から納税までのスケジュールの相談</li>
+          <li>相続税申告書の作成及び申告</li>
+          <li>贈与について</li>
+          <li>贈与を行うまでの税務関係の相談(最良の贈与の仕方等、贈与を行う前に、まずはご相談ください。)</li>
+          <li>贈与税申告書の作成及び申告</li>
+          <li>資産関係の譲渡所得についての相談、申告書の作成及び申告</li>
+        </ul>
+      </v-card-text>
+    </v-card>
+    <v-card min-height="400px">
+      <v-card-title class="headline">
+        事業継承
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li>事業承継及び事業の譲渡・買収に関する相談等</li>
+        </ul>
+      </v-card-text>
+    </v-card>
+    <v-card min-height="400px">
+      <v-card-title class="headline">
+      税務・会計
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li>税務に関する各種申告書</li>
+          <li>届出書等の作成及び申告</li>
+          <li>経理事務の指導、記帳代行</li>
+          <li>会計、給与、システムの導入指導年末調整</li>
+          <li>償却資産の申告書作成及び申告</li>
+        </ul>
+      </v-card-text>
+    </v-card>
+    <v-card min-height="400px">
+      <v-card-title class="headline">
+        相談
+      </v-card-title>
+      <v-card-text>
+        <ul>
+          <li>税務全般に関する相談</li>
+          <li>税務調査の対応</li>
+          <li>キャッシュフロー計算書の作成による管理・経営のアドバイス</li>
+          <li>(初回相談は、無料とさせていただいております。お気軽にご相談ください。)</li>
+        </ul>
+      </v-card-text>
+    </v-card>
+    <v-card>
+      <v-card-title>
+        アクセスマップ
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1612.6932379118032!2d136.17780975827426!3d36.059676463337304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8bf2a9efda78b%3A0x9d76b6518811ffe1!2z54uQ5qmL5YWs5ZyS!5e0!3m2!1sja!2sjp!4v1595750092590!5m2!1sja!2sjp"
+              width="600"
+              height="450"
+              frameborder="0"
+              style="border:0;"
+              allowfullscreen=""
+              aria-hidden="false"
+              tabindex="0">
+            </iframe>
+          </v-col>
+          <v-col>
+            <p>お急ぎのご用件の場合は下記の電話番号におかけください。</p>
+            <p>お問い合わせフォームよりご用件を頂いた場合は、メールにて回答後、お電話を差し上げます。</p>
+            <h1>連絡先</h1>
+            <Ul>
+              <li>福井県福井市xxxxxxxxx</li>
+              <li>0123-56-7890</li>
+              <li>aaaaaaaaaaaaaaaaaaaaaa@gmail.com</li>
+              <li>FAX:(123)123-5678</li>
+            </Ul>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
     </v-flex>
   </v-layout>
 </template>
